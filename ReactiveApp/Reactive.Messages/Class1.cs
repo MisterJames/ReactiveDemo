919fb603934edf8a444ccda6a1c12475eabd308f
Simon Timms <stimms@gmail.com>
2014-04-06 19:46:03 -0600

    Adding db and start of messages

---
+++ ReactiveApp/Reactive.Messages/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reactive.Messages
+{
+    public class AddItemToCart
+    {
+        public Guid ItemId { get; set; }
+        public Guid CartId { get; set; }
+        public string ItemName { get; set; }
+        public string Quantity { get; set; }
+    }
+}
